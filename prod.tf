b9c17ea8cb5966894e3236ba11c43c3b1b5f70bd
Nicholas Knight <nicholas.knight@Levvels-MacBook-Pro.local>
2021-05-18 15:17:48 -0400

    Refactored to use ASG

--- prod.tf
+++ prod.tf
@@ -11,14 +11,14 @@ resource "aws_s3_bucket" "prod_tf_course" {
 resource "aws_default_vpc" "default" {}
 
 resource "aws_default_subnet" "default_az1" {
-  availability_zone = "us-east-1c"
+  availability_zone = "us-east-1a"
   tags = {
     "Terraform" : "true"
   }
 }
 
 resource "aws_default_subnet" "default_az2" {
-  availability_zone = "us-east-1a"
+  availability_zone = "us-east-1b"
   tags = {
     "Terraform" : "true"
   }
@@ -52,35 +52,8 @@ resource "aws_security_group" "prod_web" {
   }
 }
 
-resource "aws_instance" "prod_web" {
-  count = 2
-
-  ami		= "ami-0bb9f46119d70d211"
-  instance_type	= "t2.nano"
-
-  vpc_security_group_ids = [
-    aws_security_group.prod_web.id
-  ]
-
-  tags = {
-    "Terraform" : "true"
-  }
-}
-
-resource "aws_eip_association" "prod_web" {
-  instance_id 	= aws_instance.prod_web[0].id
-  allocation_id	= aws_eip.prod_web.id
-}
-
-resource "aws_eip" "prod_web" {
-  tags = {
-    "Terraform" : "true"
-  }
-}
-
 resource "aws_elb" "prod_web" {
   name 		  = "prod-web"
-  instances 	  = aws_instance.prod_web.*.id
   subnets	  = [aws_default_subnet.default_az1.id, aws_default_subnet.default_az2.id]
   security_groups = [aws_security_group.prod_web.id]
 
@@ -93,4 +66,35 @@ resource "aws_elb" "prod_web" {
   tags = {
     "Terraform" : "true"
   }
+}
+
+resource "aws_launch_template" "prod_web" {
+  name_prefix   = "prod-web"
+  image_id      = "ami-0bb9f46119d70d211"
+  instance_type = "t2.micro"
+  tags = {
+    "Terraform" : "true"
+  }
+}
+
+resource "aws_autoscaling_group" "prod_web" {
+  vpc_zone_identifier = [aws_default_subnet.default_az1.id,aws_default_subnet.default_az2.id]
+  desired_capacity    = 1
+  max_size            = 1
+  min_size            = 1
+
+  launch_template {
+    id      = aws_launch_template.prod_web.id
+    version = "$Latest"
+  }
+  tag {
+    key	  		= "Terraform"
+    value 		= "true"
+    propagate_at_launch = true
+  }
+}
+
+resource "aws_autoscaling_attachment" "prod_web" {
+  autoscaling_group_name = aws_autoscaling_group.prod_web.id
+  elb                    = aws_elb.prod_web.id
 }
